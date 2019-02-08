4d0208155746ff65551289266186da807385a475
Carlos Rodriguez <carlosrdz_16@hotmail.com>
2019-02-08 16:08:31 -0600

    plantilla

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include_once('includes/head.php');
+include_once('includes/menu.php');
+?>
+<div class="container-fluid" style="background: #eee;">
+</div>
+<?php
+include_once('includes/footer.php');
+?>
