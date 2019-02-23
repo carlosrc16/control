a5ea4ead520ab210ec2536b5851ddb0a13d6215d
Carlos Rodriguez <carlosrdz_16@hotmail.com>
2019-02-22 19:17:39 -0600

    forms cats

---
+++ paginas/adcatadmn.php
@@ -0,0 +1,39 @@
+<!--<h4>Añadir Categoria</h4>-->
+<div class="row" style="padding: 0px; width: 100%; padding-top: 15px;">
+<!--<div class="col-md-12">
+   
+
+
+   <label class="txt">Descripcion:</label>
+   <form method="get"  class="form-inline justify-content-center">
+               <div class="form-group mx-sm-3 mb-2">
+                  <input type="text" name="q" value="" class="form-control input-lg" placeholder="Número de unidad, ejemplo 84936" style="width:320px">
+               </div>
+               <button class="btn btn-primary mb-2" style="color: #fff;"><i class="fas fa-search"></i></button>           
+            </form>
+</div>-->
+
+
+
+<div class="col-md-12">
+   <div class="card bg-light mb-3">
+      <div class="card-header">
+         Categorias Gastos Administrativos<span class="" style="float: right;"><i class="fas fa-plus"></i></span>
+      </div>
+      <!--<div class="card-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem totam facere consequatur, sit illo, molestiae labore autem omnis architecto quisquam provident. Sit dignissimos, reprehenderit accusamus? Necessitatibus eligendi aut eius nam.</div>-->
+      <div class="card-body" style="padding: 0px;">
+         <table class="table table-striped table-bordered table-sm " style="padding: 0px; margin: 0px;">
+               <thead>
+                  <th>Clave</th>
+                  <th>Descripcion</th>
+                  <th>Monto</th>
+                  <th>Fecha</th>
+               </thead>
+               <tbody>
+                  
+               </tbody>
+            </table>
+      </div>
+   </div>
+</div>
+</div>
--- paginas/movs.php
+++ paginas/movs.php
@@ -13,13 +13,48 @@
 <div class="tab-content container-fluid" id="myTabContent">
 	
   <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-	<div class="container-fluid justify-content-center " style="padding-top: 10px;">
+	<div class="container-fluid " style="padding-top: 10px;">
 		<div class="" align="center" style="padding-top: 15px;">
-			<div class="container-fluid" style="padding: 0px;">
+			<div class="container-fluid" style="padding: 0px;" style="background: #eee;">
 				<p class="float-right">				
-					<a href="/crm/?comercial=solicitud-anuncio&lista=me" class="btn btn-success"><i class="fas fa-plus"></i> Categorias</a>				
+					<a href="/control/?submenu=1&movs=1&add=gastoAdmin" class="btn btn-success"><i class="fas fa-plus"></i> Categorias</a>				
 				</p>
 				<p class="lead lead-info float-left">Gastos Administrativos</p>
+
+							
+			</div>
+		</div>
+
+		
+	</div>
+	<div class="container-fluid" style="padding-top: 50px; width: 100%;">		
+		<?php 
+		if (isset($_GET['add']) && $_GET['add'] == 'gastoAdmin') {
+			include_once('paginas/adcatadmn.php');
+			exit();
+		} 
+		?>
+	</div>
+
+	<div class="container-fluid" style="padding-top: 50px; width: 100%;">
+		<div class="card bg-light mb-3" style="width: 100%;">
+			<div class="card-header">
+	         Listado de Gastos<span class="" style="float: right;"><i class="fas fa-plus"></i></span>
+	      </div>
+			<div class="card-body" style="padding: 0px;">
+				<!--<h5 class="card-title">Light card title</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+				<table class="table table-striped table-bordered table-sm " style="padding: 0px; margin: 0px;">
+					<thead>
+						<th>Clave</th>
+						<th>Descripcion</th>
+						<th>Monto</th>
+						<th>Fecha</th>
+					</thead>
+					<tbody>
+						
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
